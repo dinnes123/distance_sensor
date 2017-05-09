9423c21ed7874c0e13691b71482d7ada1262925c
dinnes123 <dinnesdenver@gmail.com>
2017-05-09 21:33:35 +0800

    Add files via upload

---
+++ distance_sensor/sensor_laser_test/sensor_laser_test.ino
@@ -0,0 +1,22 @@
+#include "distance_sensor.h"
+int i=0;
+laser_VL53L0X laser_VL53L0X_1(255,5);
+void setup() 
+    {
+        Serial.begin(9600);
+        laser_VL53L0X_1.setup(255,5);
+    }
+void loop() 
+    {
+         
+             // (int Address , int pin )
+         Serial.print(  "distance=" ) ; Serial.print( laser_VL53L0X_1.get_distance (1,500,600) ); 
+         Serial.print(  "  Is_set_base=" ) ; Serial.println( laser_VL53L0X_1.Is_set_base  );
+         
+         i++;
+         if(i>100)
+            {
+              i=0;
+              laser_VL53L0X_1.reset_base();
+            } 
+    }
